c0761167629628c138294d8843a21202b04b3114
Dinesh Uyyala <dineshuyyala1@gmail.com>
2025-06-21 13:45:01 +0530

    changed backend api

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://your-production-api-url/api'
+  apiUrl: 'https://job-application-tracker-backend-b1su.onrender.com/api'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://job-application-tracker-backend-b1su.onrender.com/api'
+  apiUrl: 'https://job-application-tracker-backend-b1su.onrender.com/api',
+  // apiUrl: 'http://localhost:3000/api',
 };
